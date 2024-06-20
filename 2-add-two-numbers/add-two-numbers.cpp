e38c7aecc31390110ce0feeea7c0ac0d14763201
ADITYAKUMARDAS6 <164795498+ADITYAKUMARDAS6@users.noreply.github.com>
2024-06-20 22:43:05 +0530

    Time: 27 ms (27.66%) | Memory: 76.1 MB (61.67%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.cpp
+++ 2-add-two-numbers/add-two-numbers.cpp
@@ -1,4 +1,3 @@
-
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -9,46 +8,26 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-class Solution {    
-    void insertAtTail(ListNode*& head, ListNode*& tail, int data) {
-        ListNode* node = new ListNode(data);
-        if (head == NULL) {
-            head = node;
-            tail = node;
-        } else {
-            tail->next = node;
-            tail = node;
-        }
-    }
-    ListNode* addNodes(ListNode*&first, ListNode*&second){ 
-        
-        ListNode* ansHead=NULL;
-        ListNode* ansTail=NULL;
-        int carry=0;
-        
-        while(first!=NULL || second!=NULL || carry!=0){
-            int val1=0;
-            if(first!=NULL){
-                val1=first->val;
-            }
-            int val2=0;
-            if(second!=NULL){
-                val2=second->val;
-            }
-            int sum=carry+val1+val2;
-            int digit=sum%10;
-            insertAtTail(ansHead,ansTail,digit);
-            carry=sum/10;
-            
-            if(first!=NULL)first=first->next;
-            if(second!=NULL)second=second->next;
-        }
-        return ansHead;
-    }
+class Solution {
 public:
-    ListNode* addTwoNumbers(ListNode* first, ListNode* second) {       
-        ListNode* ans=addNodes(first,second);
-        return ans;
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* head = new ListNode();
+        ListNode* temp = head;
+        int carry=0;
 
-    }    
+        while(l1 || l2 || carry){
+            int val1 = l1 ? l1->val : 0;
+            int val2 = l2 ? l2->val : 0;
+            int sum = val1 + val2 + carry;
+
+            temp->next = new ListNode(sum%10);
+            temp=temp->next;
+            carry=sum/10;
+
+            l1 = l1 ? l1->next : NULL;
+            l2 = l2 ? l2->next : NULL;
+        }
+
+        return head->next;
+    }
 };
