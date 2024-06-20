b7a0aab51e278c52d2e3c1b05137af3d58e2d275
ADITYAKUMARDAS6 <164795498+ADITYAKUMARDAS6@users.noreply.github.com>
2024-06-20 22:42:26 +0530

    Time: 21 ms (56.46%), Space: 76 MB (86.15%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -1,3 +1,4 @@
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -8,28 +9,46 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-class Solution {
-public:
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode *dummy=new ListNode();
-        ListNode *temp=dummy;
-        int carry =0;
-        while(l1!=NULL || l2!=NULL || carry ==1){
-            int sum=0;
-            if(l1 !=NULL){
-                sum+=l1->val;
-                l1=l1->next;
-            }
-            if(l2 !=NULL){
-                sum +=l2->val;
-                l2=l2->next;
-            }
-            sum+=carry;
-            carry =sum/10;
-            ListNode*node=new ListNode(sum%10);
-            temp->next=node;
-            temp=temp->next;
+class Solution {    
+    void insertAtTail(ListNode*& head, ListNode*& tail, int data) {
+        ListNode* node = new ListNode(data);
+        if (head == NULL) {
+            head = node;
+            tail = node;
+        } else {
+            tail->next = node;
+            tail = node;
         }
-        return dummy->next;
     }
+    ListNode* addNodes(ListNode*&first, ListNode*&second){ 
+        
+        ListNode* ansHead=NULL;
+        ListNode* ansTail=NULL;
+        int carry=0;
+        
+        while(first!=NULL || second!=NULL || carry!=0){
+            int val1=0;
+            if(first!=NULL){
+                val1=first->val;
+            }
+            int val2=0;
+            if(second!=NULL){
+                val2=second->val;
+            }
+            int sum=carry+val1+val2;
+            int digit=sum%10;
+            insertAtTail(ansHead,ansTail,digit);
+            carry=sum/10;
+            
+            if(first!=NULL)first=first->next;
+            if(second!=NULL)second=second->next;
+        }
+        return ansHead;
+    }
+public:
+    ListNode* addTwoNumbers(ListNode* first, ListNode* second) {       
+        ListNode* ans=addNodes(first,second);
+        return ans;
+
+    }    
 };
