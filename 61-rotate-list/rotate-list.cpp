f77cd72329b90422897e006385fcbf6e91459426
ADITYAKUMARDAS6 <164795498+ADITYAKUMARDAS6@users.noreply.github.com>
2024-06-21 16:59:21 +0530

    Time: 3 ms (83.67%) | Memory: 15.2 MB (26.40%) - LeetSync

--- 61-rotate-list/rotate-list.cpp
+++ 61-rotate-list/rotate-list.cpp
@@ -1,26 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     ListNode* rotateRight(ListNode* head, int k) {
-        // special case
-        if(head==0) return NULL;
-        // finding the length og linked list
-        int length=0;
-        ListNode*temp=head;
-        ListNode*tail=NULL;
-        while(temp!=NULL){
-            if(temp->next==NULL) tail=temp;
-            temp=temp->next;
-            length++;
-        }
-        k= k % length;
-        // I have to place temp at (n-k)th position
-        temp=head;
-        for(int i=1;i<(length-k);i++){
-            temp=temp->next;
-        }
-        tail->next=head;
-        head=temp->next;
-        temp->next=NULL;
-        return head;
+   if(head == NULL||head->next == NULL||k == 0) return head;
+    //calculating length
+    ListNode* temp = head;
+    int length = 1;
+    while(temp->next != NULL) {
+        ++length;
+        temp = temp->next;
+    }
+    //link last node to first node
+    temp->next = head;
+    k = k%length; //when k is more than length of list
+    int end = length-k; //to get end of the list
+    while(end--) temp = temp->next;
+    //breaking last node link and pointing to NULL
+    head = temp->next;
+    temp->next = NULL;
+        
+    return head;
+
     }
 };
