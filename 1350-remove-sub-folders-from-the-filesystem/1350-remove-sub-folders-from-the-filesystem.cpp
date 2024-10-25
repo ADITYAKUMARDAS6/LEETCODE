1fae7e4dd0ead1d425d55aeff12d66435d7d4a1e
ADITYAKUMARDAS6 <164795498+ADITYAKUMARDAS6@users.noreply.github.com>
2024-10-25 10:57:41 +0530

    Time: 52 ms (93.68%), Space: 47.8 MB (75.82%) - LeetHub

--- 1350-remove-sub-folders-from-the-filesystem/1350-remove-sub-folders-from-the-filesystem.cpp
+++ 1350-remove-sub-folders-from-the-filesystem/1350-remove-sub-folders-from-the-filesystem.cpp
@@ -16,7 +16,7 @@ public:
             lastFolder.push_back('/');
 
             // Compare current folder with last added folder
-            // compare(0, lastFolder.size(), lastFolder) checks if folder[i] starts with lastFolder
+            // compare(0, lastFolder.size(), lastFolder) checks if folder[i]     starts with lastFolder
             // If it doesn't start with lastFolder (returns != 0), then it's not a subfolder
             if(folder[i].compare(0, lastFolder.size(), lastFolder) != 0) {
                 // If not a subfolder, add to result
